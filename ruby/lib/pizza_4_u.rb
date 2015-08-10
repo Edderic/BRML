74df711c9f0cc1c5368b8b9a6ad09b199ace39d6
Edderic Ugaddan <edderic@gmail.com>
2015-08-10 07:16:20 -0400

    pizza4u with probability correct

--- ruby/lib/pizza_4_u.rb
+++ ruby/lib/pizza_4_u.rb
@@ -1,17 +1,74 @@
 require 'utils'
 
 class Pizza4U
+  attr_reader :combinations, :unique_combinations, :combinations_size, :unique_absolute_freqs
+
   def initialize
     @utils = Utils.new(4)
+    @combinations = @utils.combinations(7)
+    @unique_combinations = combinations.map {|combination| combination.sort}.uniq
+
+    @combinations_size = combinations.count
+    @unique_absolute_freqs = unique_combinations.inject({}) do |accum, uniq_combo|
+      accum[uniq_combo.join] = combinations.select{|combination| same_unique_combo(combination, uniq_combo)}.count
+      accum
+    end
   end
 
-  def unique_combinations
-    combinations.map {|combination| combination.sort}.uniq
+  def accuracy(num_times)
+    score(num_times) / num_times.to_f
+  end
+
+  def prob_correct
+    corrects_of_joint_distribution.inject(0) do |accum, hash|
+      accum = accum + hash[:posterior_prob]
+      accum
+    end
   end
 
   private
 
-  def combinations
-    @utils.combinations(7)
+  def corrects_of_joint_distribution
+    joint_distribution.select{|item| item[:correct] == true}
+  end
+
+  def joint_distribution
+    unique_absolute_freqs.inject([]) do |accum_1, (chef_k,chef_v)|
+      accum_1 + unique_absolute_freqs.inject([]) do |accum_2, (customers_k,customers_v)|
+
+      posterior_prob = chef_v / combinations_size.to_f * customers_v / combinations_size.to_f
+      hash = { correct: false, posterior_prob: posterior_prob}
+
+      if chef_k == customers_k
+        hash[:correct] = true
+      end
+
+      accum_2 << hash
+    end
+    end
+  end
+
+  def score(num_times)
+    r = Random.new
+
+    (0..num_times-1).inject(0) do |accum, count|
+      customers_pick = pick(r)
+      chefs_pick = pick(r)
+
+      if same_unique_combo(customers_pick, chefs_pick)
+        accum = accum + 1
+      else
+        accum
+      end
+    end
+  end
+
+  def same_unique_combo(pick1, pick2)
+    pick1.sort == pick2.sort
+  end
+
+  def pick(r)
+    pick_index = r.rand * combinations.length - 1
+    combinations[pick_index]
   end
 end
--- ruby/spec/pizza_4_u_spec.rb
+++ ruby/spec/pizza_4_u_spec.rb
@@ -10,9 +10,55 @@ describe Pizza4U do
       rearranged_1 = ['A', 'A', 'A', 'A', 'A', 'B', 'A']
       rearranged_2 = ['A', 'B', 'A', 'A', 'A', 'A', 'A']
 
+      orig_1 = ['A', 'A', 'A', 'A', 'A', 'B', 'B']
+      rearranged_3 = ['A', 'B', 'A', 'B', 'A', 'A', 'A']
+      rearranged_4 = ['B', 'B', 'A', 'A', 'A', 'A', 'A']
+
+      orig_2 = [ 'B', 'B', 'D', 'D', 'D', 'D', 'D']
+
+      # should not be included because there are only four
+      # pizzas to choose from, not 5.
+      orig_3 = [ 'E','E','E','E','E','E','E' ]
+
+
       expect(uniq_combs).to include(orig)
+      expect(uniq_combs).to include(orig_1)
+      expect(uniq_combs).to include(orig_2)
+      expect(uniq_combs).not_to include(orig_3)
       expect(uniq_combs).not_to include(rearranged_1)
+      expect(uniq_combs).not_to include(rearranged_2)
+      expect(uniq_combs).not_to include(rearranged_3)
+      expect(uniq_combs).not_to include(rearranged_4)
       expect(uniq_combs.count).to eq 120
+    end
+  end
+
+  describe '#accuracy(num_times)' do
+    it 'picks a random combination of pizzas (ordered by people) and compares it with another randomly-picked combination of pizzas (picked by the chef) and gives us a score' do
+      pizza_4_u = Pizza4U.new
+      accuracy = pizza_4_u.accuracy(1000000)
+
+      expect(accuracy).to eq 1/120.0
+    end
+  end
+
+  describe '#unique_absolute_freqs' do
+    it 'should return a dictionary with unique combinations as keys and absolute freqs as values' do
+
+      pizza_4_u = Pizza4U.new
+      unique_absolute_freqs = pizza_4_u.unique_absolute_freqs
+
+      total_count =  unique_absolute_freqs.inject(0){|accum,(k,v)| accum = accum + v; accum}
+      expect(total_count).to eq 16384
+      expect(unique_absolute_freqs['AAAAAAA']).to be 1
+      expect(unique_absolute_freqs['AAAAAAB']).to be 7
+    end
+  end
+
+  describe '#prob_correct' do
+    it 'gives us the chance that the chef was correct' do
+      pizza_4_u = Pizza4U.new
+      expect(pizza_4_u.prob_correct.round(3)).to eq 0.018383.round(3)
     end
   end
 end
