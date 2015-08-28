dcb05e89ae166cbc17d955bd1d5d1bcdc0708196
Edderic Ugaddan <edderic@gmail.com>
2015-08-27 20:51:17 -0400

    fix vertical specs

--- ruby/spec/battleship/vertical_spec.rb
+++ ruby/spec/battleship/vertical_spec.rb
@@ -2,45 +2,34 @@ require 'spec_helper'
 
 describe Battleship::VerticalShip do
   before do
-    @starting_point = Battleship::Point.new(1,1)
-    @table = (1..3).map do |row|
-      (1..3).map do |col|
-        Battleship::Point.new(row,col)
-      end
-    end
-
-    def @table.row_length; 3; end
-    def @table.col_length; 3; end
-    def @table.point_at(row, col)
-      self[row-1][col-1]
-    end
-    @table[2][2].miss!
+    @starting_point = Battleship::Point.new(row: 1,col: 1)
+    ships = []
+    misses = [Battleship::Point.new(row: 2, col: 2)]
+    hash = {row_length: 3, col_length: 3, ships: ships, misses: misses}
+    @table = Battleship::Table.new(hash)
 
     def has_point(some_point, occupied_points)
-      occupied_points.any? {|point| point.row == some_point.row && point.col == some_point.col}
+      occupied_points.any? {|point| point.same_as? some_point }
     end
   end
 
   describe '#occupied_points' do
     it 'returns a list of occupied points' do
-      def @table.point_at(row, col)
-        self[row-1][col-1]
-      end
       vertical_ship = Battleship::VerticalShip.new(length: 2,
                                                        table: @table,
                                                        starting_point: @starting_point)
 
       occupied_points = vertical_ship.occupied_points
 
-      expect(has_point(Battleship::Point.new(1,1), occupied_points)).to eq true
-      expect(has_point(Battleship::Point.new(2,1), occupied_points)).to eq true
-      expect(has_point(Battleship::Point.new(3,1), occupied_points)).to eq false
+      expect(has_point(Battleship::Point.new(row: 1,col: 1), occupied_points)).to eq true
+      expect(has_point(Battleship::Point.new(row: 2,col: 1), occupied_points)).to eq true
+      expect(has_point(Battleship::Point.new(row: 3,col: 1), occupied_points)).to eq false
     end
   end
 
   describe '#occupies_a_missed_point?' do
     it 'returns true if ship occupies a missed point' do
-      starting_point = Battleship::Point.new(2,3)
+      starting_point = Battleship::Point.new(row: 2,col: 2)
       vertical_ship = Battleship::VerticalShip.new(length: 2,
                                                        table: @table,
                                                        starting_point: starting_point)
@@ -48,7 +37,7 @@ describe Battleship::VerticalShip do
     end
 
     it 'returns false if ship does not occupy a missed point' do
-      starting_point = Battleship::Point.new(1,1)
+      starting_point = Battleship::Point.new(row: 1,col: 1)
       vertical_ship = Battleship::VerticalShip.new(length: 2,
                                                        table: @table,
                                                        starting_point: starting_point)
@@ -76,7 +65,7 @@ describe Battleship::VerticalShip do
                                                    starting_point: @starting_point)
       expect(vertical_ship).to be_fully_onboard
 
-      vertical_ship.start_at(Battleship::Point.new(2,1))
+      vertical_ship.start_at(Battleship::Point.new(row: 2,col: 1))
       expect(vertical_ship).to be_fully_onboard
 
     end
@@ -86,7 +75,7 @@ describe Battleship::VerticalShip do
                                                    orientation: :vertical,
                                                    table: @table,
                                                    starting_point: @starting_point)
-      vertical_ship.start_at(Battleship::Point.new(3,1))
+      vertical_ship.start_at(Battleship::Point.new(row: 3,col: 1))
       expect(vertical_ship).not_to be_fully_onboard
     end
   end
