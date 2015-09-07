a02a77c6ef797dd63b263caa985bafb34faeec2c
Edderic Ugaddan <edderic@gmail.com>
2015-09-07 14:11:02 -0400

    test another condition for sink_pair#num_times_matching_sink_pair

--- ruby/spec/battleship/sink_pair_spec.rb
+++ ruby/spec/battleship/sink_pair_spec.rb
@@ -3,6 +3,37 @@ require 'spec_helper'
 describe Battleship::SinkPair do
   describe 'with a table' do
     describe '#num_times_matching_sink_pair' do
+      describe 'when there are two ships of the same length' do
+        describe 'and both ships satisfies the length constraint' do
+          it 'should return 1' do
+            hit_1_6 = Battleship::Point.new(row: 1, col: 6, state: :hit)
+            hit_1_7 = Battleship::Point.new(row: 1, col: 7)
+
+            hits = [hit_1_6]
+            ship_length_2 = Battleship::HorizontalShip.new(length: 2)
+            another_ship_length_2 = Battleship::HorizontalShip.new(length: 2)
+            ships = [ship_length_2, another_ship_length_2]
+            sink_pair = Battleship::SinkPair.new(ship_length:2,
+                                                 point: hit_1_7)
+            sink_pairs = [sink_pair]
+            hash = {col_length: 10,
+                    row_length: 1,
+                    ships: ships,
+                    sink_pairs: sink_pairs,
+                    hits: hits}
+
+            table = Battleship::Table.new(hash)
+            # a Table should know about sink pairs, so that we can
+            # figure out what sinking should be done.
+            # However...
+            # sinking should be deferred to some other object later
+            # table.sink!(hit_1_7, 2)
+
+            expect(sink_pair.num_times_matching_sink_pair).to eq 1
+          end
+        end
+      end
+
       describe 'when the setup is not ambiguous' do
         it 'should return 1' do
           hit_1_6 = Battleship::Point.new(row: 1, col: 6, state: :hit)
@@ -22,6 +53,10 @@ describe Battleship::SinkPair do
                   hits: hits}
 
           table = Battleship::Table.new(hash)
+          # a Table should know about sink pairs, so that we can
+          # figure out what sinking should be done.
+          # However...
+          # sinking should be deferred to some other object later
           # table.sink!(hit_1_7, 2)
 
           expect(sink_pair.num_times_matching_sink_pair).to eq 1
@@ -48,13 +83,14 @@ describe Battleship::SinkPair do
                   hits: hits}
 
           table = Battleship::Table.new(hash)
-          table.sink!(sink_1_7, 2)
+          # table.sink!(sink_1_7, 2)
 
           expect(sink_pair.num_times_matching_sink_pair).to eq 2
         end
       end
     end
   end
+
   describe '#ambiguous?' do
     describe 'when the associated ship can cover the sink point more than one way' do
       it 'should return true' do
